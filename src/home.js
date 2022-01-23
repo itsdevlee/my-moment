b177a75cf6cb428565d106fe139bd0ee324452c2
je9996 <je9996@naver.com>
2022-01-23 17:00:56 +0900

    backgground 경로 바꿈

--- src/home.js
+++ src/home.js
@@ -1,3 +1,8 @@
 const user = localStorage.getItem("id");
 const h1 = document.querySelector(".title h1");
 h1.innerText = `Hello ${user}'s World`;
+
+const imgArray = ["1", "2"];
+const random = Math.floor(Math.random() * imgArray.length) + 1;
+console.log(random);
+document.body.style.backgroundImage = `url(img/${random}.gif)`;
