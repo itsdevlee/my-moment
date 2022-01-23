6be10011ae63deed808624f8684404b351dc579e
je9996 <je9996@naver.com>
2022-01-23 16:28:02 +0900

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
 const imgArray = ["1", "2"];
 const random = Math.floor(Math.random() * imgArray.length) + 1;
 console.log(random);
-document.body.style.backgroundImage = `url(../img/${random}.gif)`;
+document.body.style.backgroundImage = `url(img/${random}.gif)`;
